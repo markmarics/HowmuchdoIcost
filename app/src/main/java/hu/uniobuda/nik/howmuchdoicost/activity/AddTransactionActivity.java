e68bf59f175a074588c873abd37f0fc608a2b285
Marics Márk <maricsmark96@gmail.com>
2018-04-22 21:51:46 +0200

    AddTransactionActivity: Dátum kiválasztása működik.

--- app/src/main/java/hu/uniobuda/nik/howmuchdoicost/activity/AddTransactionActivity.java
+++ app/src/main/java/hu/uniobuda/nik/howmuchdoicost/activity/AddTransactionActivity.java
@@ -1,15 +1,64 @@
 package hu.uniobuda.nik.howmuchdoicost.activity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.text.DateFormatSymbols;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+import hu.uniobuda.nik.howmuchdoicost.MainActivity;
 import hu.uniobuda.nik.howmuchdoicost.R;
 
 public class AddTransactionActivity extends AppCompatActivity {
 
+    private static final String TAG = "AddTransactionActivity";
+
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_transaction);
+        mDisplayDate = (TextView) findViewById(R.id.textviewDate);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        AddTransactionActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,mDateSetListener,year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String date = new DateFormatSymbols().getMonths()[month]+"-"+Integer.toString(dayOfMonth)+"-"+Integer.toString(year);
+                mDisplayDate.setText(date);
+            }
+        };
+
+
+
     }
+
 }
