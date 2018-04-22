e14eb840a3b0aab21e41e83c096145793cb99e4d
Marics Márk <maricsmark96@gmail.com>
2018-04-22 16:02:16 +0200

    Alaphelyzet, DBHandler készen van.

--- app/src/main/java/hu/uniobuda/nik/howmuchdoicost/MainActivity.java
+++ app/src/main/java/hu/uniobuda/nik/howmuchdoicost/MainActivity.java
@@ -13,43 +13,13 @@ import hu.uniobuda.nik.howmuchdoicost.models.Transaction;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Transaction transaction;
-    private TextView textView;
-    DBHandler dbHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView= (TextView) findViewById(R.id.textview);
-        dbHandler= new DBHandler(this);
-        transaction=new Transaction(1,"kaja","meki",
-                2000,new Date(),"MCDONALDS",10,null);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        if(dbHandler.loadTransactions()==null){
-            if(dbHandler.insertTransaction(transaction)){
-                toastMessage("Data successfully Inserted");
-
-            } else{
-                toastMessage("Something went wrong");
-            }
-        }
-        Cursor cursor = dbHandler.loadTransactions();
-
-
-        while (cursor.moveToNext()){
-
-            String name = cursor.getString(2);
-
-            stringBuilder.append(name).append("\n");
-
-        }
-        textView.setText(stringBuilder);
-
 
     }
 
-    private void toastMessage(String message){
-        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
-    }
+
 }
