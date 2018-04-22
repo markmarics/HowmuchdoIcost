30bf6033e5ed35ffa4e2c2c569eb854ea58278ad
Marics Márk <maricsmark96@gmail.com>
2018-04-22 18:41:02 +0200

    activity_main.xml alapok, új tranzakció hozzáadása activity elkészítése.

--- app/src/main/java/hu/uniobuda/nik/howmuchdoicost/MainActivity.java
+++ app/src/main/java/hu/uniobuda/nik/howmuchdoicost/MainActivity.java
@@ -1,25 +1,41 @@
 package hu.uniobuda.nik.howmuchdoicost;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Date;
 
+import hu.uniobuda.nik.howmuchdoicost.activity.AddTransactionActivity;
 import hu.uniobuda.nik.howmuchdoicost.adapters.DBHandler;
 import hu.uniobuda.nik.howmuchdoicost.models.Transaction;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button buttonTrans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonTrans = (Button)findViewById(R.id.Button_add);
+        buttonTrans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTransactionActivity();
+            }
+        });
+
     }
 
 
+    public void openTransactionActivity(){
+        Intent AddTransActivity = new Intent(this, AddTransactionActivity.class);
+        startActivity(AddTransActivity);
+    }
 }
