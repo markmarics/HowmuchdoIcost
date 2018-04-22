709c10f23309ee9e686fef8757dcaa2ac67a9f64
Marics Márk <maricsmark96@gmail.com>
2018-04-22 17:55:52 +0200

    activity_main.xml alapok, új tranzakció hozzáadása activity elkészítése.

---
+++ app/src/main/java/hu/uniobuda/nik/howmuchdoicost/activity/AddTransactionActivity.java
@@ -0,0 +1,15 @@
+package hu.uniobuda.nik.howmuchdoicost.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hu.uniobuda.nik.howmuchdoicost.R;
+
+public class AddTransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_transaction);
+    }
+}
