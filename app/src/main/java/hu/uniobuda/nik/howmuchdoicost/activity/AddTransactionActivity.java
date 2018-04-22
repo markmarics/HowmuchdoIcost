855340181a7ce267f1781fad539410e4cb2e4075
Marics Márk <maricsmark96@gmail.com>
2018-04-23 00:33:29 +0200

    AddTransactionActivity: Location lekérése működik. Gradle-k: Frissítve.

--- app/src/main/java/hu/uniobuda/nik/howmuchdoicost/activity/AddTransactionActivity.java
+++ app/src/main/java/hu/uniobuda/nik/howmuchdoicost/activity/AddTransactionActivity.java
@@ -1,6 +1,7 @@
 package hu.uniobuda.nik.howmuchdoicost.activity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.location.Address;
@@ -12,6 +13,11 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
 import java.io.IOException;
 import java.text.DateFormatSymbols;
 import java.util.Calendar;
@@ -22,10 +28,10 @@ import hu.uniobuda.nik.howmuchdoicost.MainActivity;
 import hu.uniobuda.nik.howmuchdoicost.R;
 
 public class AddTransactionActivity extends AppCompatActivity {
-
+    private static final int PLACE_PICKER_REQUEST = 1;
     private static final String TAG = "AddTransactionActivity";
 
-    private TextView mDisplayDate;
+    private TextView mDisplayDate, mDisplayPlace;
     private DatePickerDialog.OnDateSetListener mDateSetListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +64,31 @@ public class AddTransactionActivity extends AppCompatActivity {
         };
 
 
+        mDisplayPlace = (TextView) findViewById(R.id.textviewPlace);
+
+        mDisplayPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    startActivityForResult(builder.build(AddTransactionActivity.this), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(this,data);
+                mDisplayPlace.setText(place.getName());
+            }
+        }
+    }
 }
