013c21f9b3786b45c8cd54ae41bb4ed97669d640
Szabolcs Nemeth <szabolcs.nemeth.187@gmail.com>
2018-05-13 23:18:52 +0200

    Details activity done

--- app/src/main/java/hu/uniobuda/nik/howmuchdoicost/activity/DetailsActivity.java
+++ app/src/main/java/hu/uniobuda/nik/howmuchdoicost/activity/DetailsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import hu.uniobuda.nik.howmuchdoicost.R;
@@ -12,7 +13,9 @@ import hu.uniobuda.nik.howmuchdoicost.models.Transaction;
 
 public class DetailsActivity extends AppCompatActivity {
 
-    EditText editTextName;
+
+    TextView textViewTypes, textViewDate, textViewPlace, editTextName, editTextPrice, editTextComment;
+    RatingBar ratingBar;
     DBAdapter dbAdapter;
 
     @Override
@@ -20,12 +23,23 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         editTextName = findViewById(R.id.editTextName);
-
+        editTextComment = findViewById(R.id.editTextComment);
+        editTextPrice = findViewById(R.id.editTextPrice);
+        textViewDate = findViewById(R.id.textviewDate);
+        textViewPlace = findViewById(R.id.textviewPlace);
+        textViewTypes = findViewById(R.id.textViewTypes);
+        ratingBar = findViewById(R.id.rating);
 
         Intent intent = getIntent();
         int id = intent.getExtras().getInt("TRANSACTION_ID");
         dbAdapter = new DBAdapter(DetailsActivity.this);
         Transaction transaction = dbAdapter.loadTransactions().get(id);
         editTextName.setText(transaction.getName().toString(), TextView.BufferType.EDITABLE);
+        editTextComment.setText(transaction.getComment().toString(), TextView.BufferType.EDITABLE);
+        editTextPrice.setText(Double.toString(transaction.getPrice()), TextView.BufferType.EDITABLE);
+        textViewTypes.setText(transaction.getType().toString(), TextView.BufferType.EDITABLE);
+        textViewDate.setText(transaction.getDate().toString(), TextView.BufferType.EDITABLE);
+        textViewPlace.setText(transaction.getPlace().toString(), TextView.BufferType.EDITABLE);
+        ratingBar.setRating(transaction.getRating());
     }
 }
